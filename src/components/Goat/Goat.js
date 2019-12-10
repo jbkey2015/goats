f26d9fcb3c22a863106944b37a0aca6633e269a9
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-12-09 21:31:40 -0600

    free the goat

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import goatShape from '../../helpers/propz/goatShape';
 
 class Goat extends React.Component {
   static propTypes = {
     goat: goatShape.goatShape,
+    freeGoat: PropTypes.func,
+  }
+
+  freeGoatEvent = (e) => {
+    const { freeGoat, goat } = this.props;
+    e.preventDefault();
+    freeGoat(goat.id);
   }
 
   render() {
     const { goat } = this.props;
 
     return (
-      <div className="card">
+      <div className="card w-25">
         <img src={goat.imgUrl} className="card-img-top" alt="Card cap" />
         <div className="card-body">
           <h5 className="card-title">{goat.name}</h5>
           <p className="card-text">{goat.age}</p>
           <p className="card-text">{goat.description}</p>
+          <div className="btn" onClick={this.freeGoatEvent} >Free Me</div>
         </div>
       </div>
     );
